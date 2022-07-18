e4793a2c69f22f0febe2acb22c0eda62f3182506
Leonardo Zanotti <leonardojosezanotti@gmail.com>
2022-07-18 17:51:37 -0300

    Planet class

--- pyplanet.py
+++ pyplanet.py
@@ -4,22 +4,46 @@
 
 import math
 import sys
+from typing import Type
+
 import pygame
 
 pygame.init()
 SIZE = WIDTH, HEIGHT = 800, 800
 SCREEN = pygame.display.set_mode(SIZE)
+pygame.display.set_caption("Planet simulation")
 BLACK = 0, 0, 0
+
+
+class Planet:
+    AU = 149.6e9  # Astronomical Unities
+    G = 6.67428e-11
+    SCALE = 250 / AU    # 1 AU = 100 pixels
+    TIMESTEP = 3600 * 24    # 1 day
+
+    def __init__(self, x, y, radius, color, mass):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.color = color;
+        self.mass = mass
+
+        self.sun = False
+        self.distance_to_sun = 0
+        self.orbit = []
+
+        self.x_vel = 0
+        self.y_vel = 0
 
 
 def main():
     run = True
-
-    pygame.display.set_caption("Planet simulation")
+    clock = pygame.time.Clock()
 
     while run:
+        clock.tick(60)
         SCREEN.fill(BLACK)
-        pygame.display.flip()
+        pygame.display.update()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -29,4 +53,3 @@ def main():
 
 
 main()
-
