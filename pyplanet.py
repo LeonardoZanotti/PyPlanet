5c3c3adfdb0ad1012fbe25d3f56f4dfb829601e2
Leonardo Zanotti <leonardojosezanotti@gmail.com>
2022-07-18 19:14:36 -0300

    planets doing planet stuff

--- pyplanet.py
+++ pyplanet.py
@@ -90,6 +90,11 @@ def main():
     mercury = Planet(0.387 * Planet.AU, 0, 8, DARK_GREY, 3.3 * 10**23)
     venus = Planet(0.723 * Planet.AU, 0, 14, WHITE, 4.8685 * 10**24)
 
+    earth.y_vel = 29.783 * 1000
+    mars.y_vel = 24.077 * 1000
+    mercury.y_vel = 47.4 * 1000
+    venus.y_vel = -35.02 * 1000
+
     planets = [sun, earth, mars, mercury, venus]
 
     while 1:
